2d6e31a62bc843f913380771c4d3b5cb6b7859fe
benjamski <benjadamski@gmail.com>
2016-12-13 14:12:40 -0500

    Define file object
    -aws-upload

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -5,3 +5,10 @@ const fs = require('fs');
 const AWS = require('aws-sdk');
 // invokes constructor function to create new AWS module
 const s3 = new AWS.S3();
+
+let file = {
+ path: process.argv[2],
+ title: process.argv[3]
+};
+
+console.log("file is ", file);
